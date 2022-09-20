684df8cd303013ba7bb0666ffecc298ce37c9a6f
Raghad-Mezied <76119348+Raghad-Mezied@users.noreply.github.com>
2022-09-20 23:20:15 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,35 +1,12 @@
-import React, { Component } from "react";
+import React from "react";
+import UserCard from "./components/UserCard";
 
-export default class app extends Component {
-  constructor() {
-    console.log("constructor");
-    super();
-    this.state = {
-      title: "Chart Title",
-      xValues: [],
-      yValues: [],
-      type: "line",
-      url: "http://www.letsracethailand.com/extensions/themes/gae_letracethailand101/staticfiles/images/placeholder-image.png",
-    };
-  }
+const App = () => {
+  return (
+    <div>
+      <UserCard />
+    </div>
+  );
+};
 
-  componentDidMount() {
-    console.log("componentDid mount");
-    this.setState({ title: "lapalap" });
-    throw new Error("err");
-  }
-
-  shouldComponentUpdate() {
-    console.log("update");
-    return true;
-  }
-
-  componentDidCatch(err, info) {
-    console.log("catch err", err, info);
-  }
-
-  render() {
-    console.log("render");
-    return <div>app</div>;
-  }
-}
+export default App;
